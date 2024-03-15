83653e8ad06f03e8c9169b7416ff523d22d06590
shima8823 <taiyaki1275@gmail.com>
2024-03-15 18:28:31 +0900

    refactor: c to c++

--- srcs/load_bmp.cpp
+++ srcs/load_bmp.cpp
@@ -7,104 +7,51 @@
 #include "load.hpp"
 
 GLuint loadBMP_custom(const char *imagepath) {
+  std::cout << "Reading image " << imagepath << std::endl;
 
-  printf("Reading image %s\n", imagepath);
+  std::ifstream file(imagepath, std::ios::binary);
+  if (!file.is_open()) {
+    std::cerr << imagepath << "Failed to open." << std::endl;
+    return 0;
+  }
 
-  // Data read from the header of the BMP file
   unsigned char header[54];
-  unsigned int dataPos;
-  unsigned int imageSize;
-  unsigned int width, height;
-  // Actual RGB data
-  unsigned char *data;
-
-  // Open the file
-  FILE *file = fopen(imagepath, "rb");
-  if (!file) {
-    printf("%s could not be opened. Are you in the right directory ? Don't "
-           "forget to read the FAQ !\n",
-           imagepath);
-    getchar();
+  file.read(reinterpret_cast<char *>(header), 54);
+  if (file.gcount() != 54 || header[0] != 'B' || header[1] != 'M') {
+    std::cerr << "Not a correct BMP file\n";
     return 0;
   }
 
-  // Read the header, i.e. the 54 first bytes
+  // reinterpret_cast is used to convert the pointer to a different type
+  unsigned int dataPos = *reinterpret_cast<unsigned int *>(&header[0x0A]);
+  unsigned int imageSize = *reinterpret_cast<unsigned int *>(&header[0x22]);
+  unsigned int width = *reinterpret_cast<unsigned int *>(&header[0x12]);
+  unsigned int height = *reinterpret_cast<unsigned int *>(&header[0x16]);
 
-  // If less than 54 bytes are read, problem
-  if (fread(header, 1, 54, file) != 54) {
-    printf("Not a correct BMP file 1\n");
-    fclose(file);
-    return 0;
-  }
-  // A BMP files always begins with "BM"
-  if (header[0] != 'B' || header[1] != 'M') {
-    printf("Not a correct BMP file 2\n");
-    fclose(file);
-    return 0;
-  }
-  // Make sure this is a 24bpp file
-  if (*(int *)&(header[0x1E]) != 0) {
-    printf("Not a correct BMP file 3\n");
-    fclose(file);
-    return 0;
-  }
-  if (*(int *)&(header[0x1C]) != 24) {
-    printf("Not a correct BMP file 4\n");
-    fclose(file);
-    return 0;
-  }
-
-  // Read the information about the image
-  dataPos = *(int *)&(header[0x0A]);
-  imageSize = *(int *)&(header[0x22]);
-  width = *(int *)&(header[0x12]);
-  height = *(int *)&(header[0x16]);
-
-  // Some BMP files are misformatted, guess missing information
   if (imageSize == 0)
-    imageSize = width * height *
-                3; // 3 : one byte for each Red, Green and Blue component
+    imageSize = width * height * 3;
   if (dataPos == 0)
-    dataPos = 54; // The BMP header is done that way
+    dataPos = 54;
 
-  // Create a buffer
-  data = new unsigned char[imageSize];
+  std::vector<unsigned char> data(imageSize);
 
-  // Read the actual data from the file into the buffer
-  fread(data, 1, imageSize, file);
+  file.seekg(dataPos);
+  file.read(reinterpret_cast<char *>(data.data()), imageSize);
+  file.close();
 
-  // Everything is in memory now, the file can be closed.
-  fclose(file);
-
-  // Create one OpenGL texture
   GLuint textureID;
   glGenTextures(1, &textureID);
-
-  // "Bind" the newly created texture : all future texture functions will modify
-  // this texture
   glBindTexture(GL_TEXTURE_2D, textureID);
-
-  // Give the image to OpenGL
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR,
-               GL_UNSIGNED_BYTE, data);
+               GL_UNSIGNED_BYTE, data.data());
 
-  // OpenGL has now copied the data. Free our own version
-  delete[] data;
-
-  // Poor filtering, or ...
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-  // ... nice trilinear filtering ...
+  // Set texture parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
-  // ... which requires mipmaps. Generate them automatically.
   glGenerateMipmap(GL_TEXTURE_2D);
 
-  // Return the ID of the texture we just created
   return textureID;
 }
-
